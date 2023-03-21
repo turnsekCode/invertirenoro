e93df9f899dc08f6ffe825da834389168a57fc22
abraham <abraham.turnsek@gmail.com>
2023-03-21 11:07:04 +0100

    cambio a etiqueta script nextja

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,18 +4,18 @@ import Script from "next/script";
 export default function Document() {
   return (
     <Html lang="en">
-      <script
+      <Script
         id="Cookiebot"
         src="https://consent.cookiebot.com/uc.js"
         data-cbid="66a968ad-70de-4703-81be-84844ed28395"
         data-blockingmode="auto"
         type="text/javascript"
-      ></script>
-      <script
+      ></Script>
+      <Script
         id="CookieDeclaration"
         src="https://consent.cookiebot.com/66a968ad-70de-4703-81be-84844ed28395/cd.js"
         type="text/javascript"
-      ></script>
+      ></Script>
       <Head />
 
       <body>
